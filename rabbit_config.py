ad4a69ecd9d497e036f7c956fac130de1b7c3063
Nathan Leach <nleach999@gmail.com>
2024-06-03 21:22:03 +0000

    setup queue schema

--- rabbit_config.py
+++ rabbit_config.py
@@ -21,6 +21,7 @@ async def setup() -> None:
         async with rmq.channel() as channel:
             scan_in_exchange = await channel.declare_exchange(WorkflowStateService.EXCHANGE_SCAN_INPUT, aio_pika.ExchangeType.FANOUT, durable=True)
             scan_await_exchange = await channel.declare_exchange(WorkflowStateService.EXCHANGE_SCAN_WAIT, aio_pika.ExchangeType.TOPIC, durable=True, internal=True)
+            scan_annotate_exchange = await channel.declare_exchange(WorkflowStateService.EXCHANGE_SCAN_ANNOTATE, aio_pika.ExchangeType.TOPIC, durable=True, internal=True)
             scan_feedback_exchange = await channel.declare_exchange(WorkflowStateService.EXCHANGE_SCAN_FEEDBACK, aio_pika.ExchangeType.TOPIC, durable=True, internal=True)
             polling_delivery_exchange = await channel.declare_exchange(WorkflowStateService.EXCHANGE_SCAN_POLLING, aio_pika.ExchangeType.TOPIC, durable=True, internal=True)
 
@@ -29,13 +30,16 @@ async def setup() -> None:
                                     arguments = {
                                         'x-dead-letter-exchange' : WorkflowStateService.EXCHANGE_SCAN_POLLING})
 
-            pr_feedback_queue = await channel.declare_queue(WorkflowStateService.QUEUE_FEEDBACK_PR, durable=True)
+            pr_feedback_queue = await channel.declare_queue(WorkflowStateService.get_pr_feedback_queue_name(moniker), durable=True)
+            pr_annotate_queue = await channel.declare_queue(WorkflowStateService.get_pr_annotate_queue_name(moniker), durable=True)
             
             await polling_scans_queue.bind(polling_delivery_exchange, WorkflowStateService.get_poll_binding_topic(moniker))
             await awaited_scans_queue.bind(scan_await_exchange, WorkflowStateService.ROUTEKEY_SCAN_WAIT)
-            await pr_feedback_queue.bind(scan_feedback_exchange, WorkflowStateService.ROUTEKEY_FEEDBACK_PR)
+            await pr_feedback_queue.bind(scan_feedback_exchange, WorkflowStateService.get_pr_feedback_binding_topic(moniker))
+            await pr_annotate_queue.bind(scan_annotate_exchange, WorkflowStateService.get_pr_annotate_binding_topic(moniker))
             await scan_await_exchange.bind(scan_in_exchange)
             await scan_feedback_exchange.bind(scan_in_exchange)
+            await scan_annotate_exchange.bind(scan_in_exchange)
 
 
 if __name__ == "__main__":
