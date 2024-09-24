ecda0c4d3e09bdbdf86d467d9b19e3a6ea0dbfa5
Nathan Leach <nleach999@gmail.com>
2024-09-24 20:13:43 +0000

    prep for pr feedback

--- scm_services/gh.py
+++ scm_services/gh.py
@@ -1,7 +1,14 @@
 from .scm import SCMService
-# from api_utils.apisession import APISession
 from scm_services.cloner import Cloner
+from typing import Dict
 
 
 class GHService(SCMService):
-    pass
+    __max_content_chars = 65535
+
+    async def exec_pr_decorate(self, organization : str, project : str, repo_slug : str, pr_number : str, scanid : str, full_markdown : str, 
+        summary_markdown : str, event_msg : Dict=None):
+        raise NotImplementedError("exec_pr_decorate")
+   
+    def create_code_permalink(self, organization : str, project : str, repo_slug : str, branch : str, code_path : str, code_line : str):
+        raise NotImplementedError("create_code_permalink")
