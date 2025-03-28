aae4edd6e92eff1f324692307383c9e71dc3b495
Nathan Leach <nleach999@gmail.com>
2025-03-28 17:50:15 +0000

    update client

--- cxoneflow_kickoff_api/kickoff_client.py
+++ cxoneflow_kickoff_api/kickoff_client.py
@@ -2,7 +2,7 @@ import jwt, time, asyncio, logging, requests
 from .exceptions import KickoffClientException
 from .signature_alg import get_signature_alg
 from cryptography.hazmat.primitives.serialization import load_ssh_private_key
-from typing import Dict
+from typing import Dict, Union, Tuple, Callable
 from .kickoff_msgs import KickoffMsg, KickoffResponseMsg
 
 
@@ -18,8 +18,36 @@ class KickoffClient:
     def log(clazz) -> logging.Logger:
         return logging.getLogger(clazz.__name__)
 
-    def __init__(self, private_ssh_key : str, private_key_password : str, 
-                 cxoneflow_ko_url : str, user_agent : str, proxies : Dict[str, str] = None, ssl_verify : bool = True):
+    def __init__(self, private_ssh_key : str, private_key_password : Union[str,None], 
+                 cxoneflow_ko_url : str, user_agent : str, 
+                 proxies : Dict[str, str] = None, ssl_verify : bool = True):
+        """A client for orchestrating initial repository scans with CxOneFlow.
+
+        Args:
+            private_ssh_key (str): The string representation of a PEM encoded private SSH key.
+
+            private_key_password (Union[str,None]): If the private key is password protected, provide the password.
+                                                    If the private key is not protected, set to "None".
+            
+            cxoneflow_ko_url (str): The URL to the CxOneFlow kickoff endpoint for any given SCM.
+
+                                    Examples for current SCM endpoints:
+                                    https://cxoneflow.corp.com/gh/kickoff
+                                    https://cxoneflow.corp.com/gl/kickoff
+                                    https://cxoneflow.corp.com/adoe/kickoff
+                                    https://cxoneflow.corp.com/bbdc/kickoff
+
+
+            user_agent (str): A user agent string, usually the name/version of your client that is
+                              sending payloads to the CxOneFlow endpoint.
+
+            proxies (Dict[str, str], optional): A dictionary that defines proxy connections as defined in the Python Requests API. Defaults to None.
+
+
+            ssl_verify (bool, optional): Set to False to skip SSL verification.  
+                                         This can also be an absolute path to a PEM encoded certificate
+                                         used to validate the SSL certificate used on the CxOneFlow endpoint. Defaults to True.
+        """
         self.__lock = asyncio.Lock()
         self.__pkey = load_ssh_private_key(private_ssh_key.encode("UTF-8"), private_key_password)
         self.__service_url = cxoneflow_ko_url
@@ -66,7 +94,28 @@ class KickoffClient:
                 return resp
 
 
-    async def kickoff_scan(self, msg : KickoffMsg) -> KickoffResponseMsg:
+    async def kickoff_scan(self, msg : KickoffMsg, 
+                           waiting_callback : Callable[[KickoffResponseMsg], bool] = None) -> Tuple[bool, Union[None, KickoffResponseMsg]]:
+        """The method that executes the scan kickoff.
+
+        Args:
+            msg (KickoffMsg): A kickoff message type appropriate for the CxOneFlow SCM endpoint where the message will be delivered.
+
+
+            waiting_callback (Callable[[KickoffResponseMsg], bool], optional): An optional method that is called
+                                                                               when the CxOneFlow endpoint indicates there are too many
+                                                                               concurrently running scans.  The client will retry the scan
+                                                                               after a delay.  The delay continues until the scan is submitted
+                                                                               or the callback returns False. Defaults to None.
+
+        Raises:
+            KickoffClientException: Throws an exception in the event there is an error when attempting to execute a scan kickoff.
+
+        Returns:
+            Tuple[bool, Union[None,KickoffResponseMsg]]: Returns True if a scan was started, False otherwise.  If the CxOneFlow endpoint
+                                                         returns a KickoffResponseMsg, it is also returned.  If no KickoffResponseMsg is
+                                                         available then None is returned.
+        """
 
         cur_sleep_delay = KickoffClient.__SLEEP_SECONDS
         retry_count = 0
@@ -83,11 +132,16 @@ class KickoffClient:
 
             if resp.status_code == 201:
                 self.log().debug(f"Scan started: {resp_msg.to_json()}")
-                return resp_msg
+                return True, resp_msg
             elif resp.status_code == 299:
                 self.log().debug(f"The server indicated a kickoff scan is already running or has finished for {msg}")
-                return resp_msg
+                return False, resp_msg
             elif resp.status_code == 429:
+
+                if waiting_callback is not None and not waiting_callback(resp_msg):
+                    self.log().debug("The callback returned False to indicate the client should not retry submission.")
+                    return False, resp_msg
+                
                 if retry_count % KickoffClient.__SLEEP_REPORT_MOD == 0:
                     self.log().info(f"The server indicated that too many concurrent scans are running." + 
                                     f"Currently running: {len(resp_msg.running_scans)}. Sleeping for {cur_sleep_delay} seconds.")
@@ -96,7 +150,8 @@ class KickoffClient:
                     for running in resp_msg.running_scans:
                         self.log().debug(running)
                     self.log().debug("Running scans list: end")
-                    
+
+                self.log().debug(f"Sleeping {cur_sleep_delay}s before retrying...")    
                 await asyncio.sleep(cur_sleep_delay)
                 cur_sleep_delay = min(KickoffClient.__SLEEP_MAX_SECONDS, cur_sleep_delay + KickoffClient.__SLEEP_SECONDS)
             else:
