921b751311c362e45b70672a48f08de280675471
Nathan Leach <203706735+cx-nathan-leach@users.noreply.github.com>
2025-05-14 19:03:40 +0000

    add execution timing logging

--- naming_services/__init__.py
+++ naming_services/__init__.py
@@ -1,7 +1,8 @@
 from scm_services import SCMService
 from api_utils.auth_factories import EventContext
 from typing import Coroutine, Tuple
-import logging
+from time import perf_counter_ns
+import logging, contextlib
 
 class ProjectNamingService:
 
@@ -15,15 +16,27 @@ class ProjectNamingService:
     self.__naming_coro = coro
     self.__scm_service = scm_service
 
+
+  @contextlib.asynccontextmanager
+  async def __call_timer(self):
+    start = perf_counter_ns()
+
+    try:
+      yield start
+    finally:
+      ProjectNamingService.log().info(f"{self.__naming_coro.__module__}.{self.__naming_coro.__name__} script execution time: {perf_counter_ns() - start}ns")
+      
+
   
   async def get_project_name(self, default_name : str, context : EventContext) -> str:
     if self.__naming_coro is None:
       return default_name
-    
-    try:
-      name = await self.__naming_coro(context, self.__scm_service)
-      return name if name is not None else default_name
-    except BaseException as ex:
-      ProjectNamingService.log().info(f"Exception thrown by naming module [{self.__naming_coro.__name__}].  Using default name [{default_name}].")
-      ProjectNamingService.log().exception(ex)
-      return default_name
+
+    async with self.__call_timer():  
+      try:
+        name = await self.__naming_coro(context, self.__scm_service)
+        return name if name is not None else default_name
+      except BaseException as ex:
+        ProjectNamingService.log().info(f"Exception thrown by naming module [{self.__naming_coro.__name__}].  Using default name [{default_name}].")
+        ProjectNamingService.log().exception(ex)
+        return default_name
