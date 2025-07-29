b4729d1bf00c479ad1a45b6ef182b348c429e7db
Nathan Leach <203706735+cx-nathan-leach@users.noreply.github.com>
2025-07-29 16:52:19 +0000

    rename scan-agent

--- agent_packages/scan-agent/debian/build-package.sh
+++ agent_packages/scan-agent/debian/build-package.sh
@@ -11,26 +11,26 @@ else
   VERSION="0.0.0"
 fi
 
-DEB_NAME=cxoneflow-resolver-agent_${VERSION}_$(dpkg-architecture -q DEB_BUILD_ARCH).deb
+DEB_NAME=cxoneflow-scan-agent_${VERSION}_$(dpkg-architecture -q DEB_BUILD_ARCH).deb
 
 echo Building $DEB_NAME
 
-mkdir -p $PACKAGE_ROOT/deb-package/etc/cxoneflow-resolver-agent
-mkdir -p $PACKAGE_ROOT/deb-package/opt/cxoneflow-resolver-agent
+mkdir -p $PACKAGE_ROOT/deb-package/etc/cxoneflow-scan-agent
+mkdir -p $PACKAGE_ROOT/deb-package/opt/cxoneflow-scan-agent
 
 cp $PACKAGE_ROOT/control $PACKAGE_ROOT/deb-package/DEBIAN
 echo "Version: $VERSION" >> $PACKAGE_ROOT/deb-package/DEBIAN/control
 
-cp $PACKAGE_ROOT/../etc/cxoneflow-resolver-agent/* $PACKAGE_ROOT/deb-package/etc/cxoneflow-resolver-agent/
-cp $PACKAGE_ROOT/../systemd/* $PACKAGE_ROOT/deb-package/opt/cxoneflow-resolver-agent/
+cp $PACKAGE_ROOT/../etc/cxoneflow-scan-agent/* $PACKAGE_ROOT/deb-package/etc/cxoneflow-scan-agent/
+cp $PACKAGE_ROOT/../systemd/* $PACKAGE_ROOT/deb-package/opt/cxoneflow-scan-agent/
 
 docker run -i --rm -w /src \
--v $SRC_ROOT:/src -v $PACKAGE_ROOT/deb-package/opt/cxoneflow-resolver-agent:/dist/output \
+-v $SRC_ROOT:/src -v $PACKAGE_ROOT/deb-package/opt/cxoneflow-scan-agent:/dist/output \
 python:3.12-bookworm sh -c \
 " \
 pip install -U pyinstaller && \
 pip install -r requirements.txt && \
-pyinstaller -F --copy-metadata aio-pika --specpath /dist/platform/spec --distpath /dist/output --workpath /dist/work resolver_agent.py \
+pyinstaller -F --copy-metadata aio-pika --specpath /dist/platform/spec --distpath /dist/output --workpath /dist/work cx_scan_agent.py \
 "
 
 dpkg-deb --build $PACKAGE_ROOT/deb-package
