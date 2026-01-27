22fc4820f2efe94f003fac891b26205e765006d8
Nathan Leach <203706735+cx-nathan-leach@users.noreply.github.com>
2026-01-27 18:52:59 +0000

    remove service variation

--- workflows/feedback_services/pr/__init__.py
+++ workflows/feedback_services/pr/__init__.py
@@ -1,6 +1,5 @@
 from .abstract_pr_service import AbstractPRFeedbackService
 from .pr_feedback_service import PRFeedbackService
-from .gh_commit_status_pr_fb_service import GithubCommitStatusPRFBService
 from .queue_constants import PRQueueConstants
 
 
